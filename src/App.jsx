38f69a5a4fd55194d709f8c1ec51a7ebdb4c9dff
pratiksha <bendreprtiksha75@gmail.com>
2025-04-14 09:59:40 +0530

    app_url

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api/users';
+const API_URL = 'https://backenddemo-three.vercel.app/api/users';
 
 const App = () => {
   const [users, setUsers] = useState([]);
